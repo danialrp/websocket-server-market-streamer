3bd0b971ce7d3d4b6a281826e022808ccf5b25f9
Danial Panah <me@danialrp.com>
2022-01-24 15:36:03 +0330

    modified cleansing pairs cron to every 2hrs + modified restart server cron to every 10min

--- src/utils/cron.ts
+++ src/utils/cron.ts
@@ -7,8 +7,8 @@ import * as marketAssets from './market';
 import * as serverRefresher from './restart-server';
 import * as pairsCleaner from './cleansing-pairs';
 
-// Every 3hrs.
-const cleansingPairs = new CronJob('0 0 */3 * * *', async function () {
+// Every 2hrs.
+const cleansingPairs = new CronJob('0 0 */2 * * *', async function () {
     await pairsCleaner.deleteLightWeightPairs();
 }, null, true, 'UTC');
 
@@ -17,8 +17,9 @@ const updateAssetMarketJob = new CronJob('0 0 8 * * *', async function () {
     await marketAssets.getAssetMarkets();
 }, null, true, 'UTC');
 
-// Every 4hrs.
-const restartServer = new CronJob('0 0 */4 * * *', async function () {
+// Every 3hrs.
+// const restartServer = new CronJob('0 0 */3 * * *', async function () {
+const restartServer = new CronJob('0 */10 * * * *', async function () {
     serverRefresher.restartServer();
 }, null, true, 'UTC');
 
