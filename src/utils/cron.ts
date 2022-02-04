5172f3b4f9beddd7eab543541d7f286abfabd24d
Danial Panah <me@danialrp.com>
2022-02-04 13:16:30 +0330

    reverted cron job scheduler to 3 hrs

--- src/utils/cron.ts
+++ src/utils/cron.ts
@@ -18,8 +18,7 @@ const updateAssetMarketJob = new CronJob('0 0 8 * * *', async function () {
 }, null, true, 'UTC');
 
 // Every 3hrs.
-// const restartServer = new CronJob('0 0 */3 * * *', async function () {
-const restartServer = new CronJob('0 */1 * * * *', async function () {
+const restartServer = new CronJob('0 0 */3 * * *', async function () {
     serverRefresher.restartServer();
 }, null, true, 'UTC');
 
